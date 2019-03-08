48640ae4396979a4b8959c2adc1bf4e3526dc139
WilliamTrojniak <william.trojniak@ucc.on.ca>
2019-03-08 01:34:54 -0500

    Developing ideas work

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,15 @@ for (i = 0; i < acc.length; i++) {
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight){
       panel.style.maxHeight = null;
+      if(document.getElementById("lastAccordion") == this) {
+        this.style.borderRadius = "0px 0px 30px 30px";
+      }
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
+
+      if(document.getElementById("lastAccordion") == this){
+        this.style.borderRadius = "0px";
+      }
     } 
   });
 }

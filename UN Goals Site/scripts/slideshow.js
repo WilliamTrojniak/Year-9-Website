e21a5bfb814b53e93c24de6a1a9f79f171aa7572
William Trojniak <william.trojniak@Y22-Design-William-Trojniak.local>
2019-04-07 20:18:55 -0400

    Un goals site

--- UN Goals Site/scripts/slideshow.js
+++ UN Goals Site/scripts/slideshow.js
@@ -1,10 +1,12 @@
 var slideIndex = 1;
 showSlides(slideIndex);
+updatePageContent();
 
 function plusSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   if (n+slideIndex <= slides.length && n+slideIndex >= 1) {
     showSlides(slideIndex += n);
+    updatePageContent();
   }
 
 }
@@ -22,8 +24,23 @@ function showSlides(n) {
 }
 
 
-function updatePage() {
+function updatePageContent() {
   var firstPage = document.getElementsByClassName("firstSlideElement");
   var secondPage = document.getElementsByClassName("secondSlideElement");
   var thirdPage = document.getElementsByClassName("thirdSlideElement");
+  var pages = [firstPage, secondPage, thirdPage];
+
+// Hide all elements from all pages
+  for(i = 0; i < pages.length; i++){ 
+    for(t = 0; t < pages[i].length; t++){
+      pages[i][t].style.display = "none";
+    }
+  }
+
+//Show active page elements
+  var activePage = slideIndex-1;
+  for(i = 0; i < pages[activePage].length; i++){
+    pages[activePage][i].style.display = "block";
+  }
+  
 }
